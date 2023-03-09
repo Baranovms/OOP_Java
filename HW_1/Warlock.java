55faa4c37577b188af7020512e6e51ba04e24ba7
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2023-03-09 19:51:54 +0300

    добавлен файл Warlock из ДЗ_1

--- HW_1/Warlock.java
+++ HW_1/Warlock.java
@@ -1,5 +1,28 @@
 package OOP_JAVA.HW_1;
 
-public class Warlock {
-    
+public class Warlock extends BaseHero {
+    public Warlock(String name) {
+        super(name, 50);
+    }
+
+    public Warlock() {
+        super();
+    }
+
+    public void curse() {
+        System.out.println(this.name + " is cursing the enemy.");
+    }
+
+    public void summonDemon() {
+        System.out.println(this.name + " is summoning a demon.");
+    }
+
+    public void step() {
+        System.out.println("Warlock walks...");
+    }
+
+    @Override
+    public String getInfo() {
+        return "Я Колдун";
+    }
 }
